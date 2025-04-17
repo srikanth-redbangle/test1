7e2af2e64bb0fbb7e7fa6c6bda5d73cddf8edb0c
srikanth-redbangle <139528901+srikanth-redbangle@users.noreply.github.com>
2025-04-17 13:15:30 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/abc',
-        destination: 'https://master.d15op9hcraoiqo.amplifyapp.com/abc',
-        permanent: false, // use true if this is permanent
-      },
-    ];
-  },
   // compiler: {
   //   removeConsole: false,
   // },
