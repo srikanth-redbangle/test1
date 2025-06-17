907ee823a245c7246964f10f8ed42822d129e157
srikanth-redbangle <139528901+srikanth-redbangle@users.noreply.github.com>
2025-06-17 12:47:25 +0530

    redirection reverted

--- next.config.js
+++ next.config.js
@@ -4,15 +4,6 @@ const nextConfig = {
   // compiler: {
   //   removeConsole: false,
   // },
-  async redirects() {
-    return [
-      {
-        source: 'https://www.dev.staging.singalong.co.in',
-        destination: 'https://dev.staging.singalong.co.in',
-        permanent: false, // use true if this is permanent
-      },
-    ];
-  },
 }
 
 module.exports = nextConfig
